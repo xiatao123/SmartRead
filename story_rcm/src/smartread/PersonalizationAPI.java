41ed1a86e9ce54e4967b7e3f7f70c4218bce18ba
xiatao123 <xiatao123@hotmail.com>
2013-01-05 13:27:13 +0800

    add code todo performance logging

--- story_rcm/src/smartread/PersonalizationAPI.java
+++ story_rcm/src/smartread/PersonalizationAPI.java
@@ -8,14 +8,25 @@ import smartread.db.DBServeEvent;
 import smartread.db.DBStory;
 import smartread.db.DBUser;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class PersonalizationAPI {
+    private static final Logger logger = LogManager.getLogger(PersonalizationAPI.class);
+    
     public List<Story> getUserStory(String uid) {
+        Long starttime = System.currentTimeMillis();
         List<Story> stories = DBStory.retrieveDefaultStory();
         User user = DBUser.retrieveUser(uid);
+        Long endtime = System.currentTimeMillis();
+        
+        logger.error("Time(ms) taken to retrive user story: "+ String.valueOf(endtime-starttime));
+        
         return calcualte(user, stories);
     }
 
     public void updateUserInterests(int lookbackMinute) {
+        Long starttime = System.currentTimeMillis();
         Map<String, List<ServeEvent>> serves = DBServeEvent.QueryEvents(lookbackMinute);
         String freq;
         if(lookbackMinute==5)
@@ -38,9 +49,12 @@ public class PersonalizationAPI {
             }
             DBUser.updateUserInterest(uid, freq, tags);
         }
+        Long endtime = System.currentTimeMillis();
+        logger.error("Time(ms) taken to update user interests for "+lookbackMinute+" minutes: "+ String.valueOf(endtime-starttime));
     }
 
     private List<Story> calcualte(User user, List<Story> stories) {
+        Long starttime = System.currentTimeMillis();
         Map<String, Double> interest = user.getInterests();
         if (interest.size() == 0)
             return stories;
@@ -49,10 +63,14 @@ public class PersonalizationAPI {
             List<String> tags = s.getTags();
             s.setScore(s.getScore()*Utils.evaluateInterest(interest, tags));
         }
+        Long endtime = System.currentTimeMillis();
+        logger.error("Time(ms) taken to calculate story point for user "+user.getUid()+": "+ String.valueOf(endtime-starttime));
         return stories;
     }
 
     public static void main(String args[]) {
+        logger.trace("Entering application.");
+        
         PersonalizationAPI api = new PersonalizationAPI();
         api.updateUserInterests(5);
 
@@ -60,5 +78,6 @@ public class PersonalizationAPI {
         for (Story s : stories) {
             System.out.println(s);
         }
+        logger.trace("Exiting application.");
     }
 }
