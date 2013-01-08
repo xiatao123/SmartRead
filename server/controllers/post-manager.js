c687efe62f42a661d6e578d7d96864c3bd97c96e
Frank Wang <frankyanwang@gmail.com>
2013-01-07 21:53:13 -0800

    Sort by pubDate. ALso fix scroll background issue by overflow hidden.

--- server/controllers/post-manager.js
+++ server/controllers/post-manager.js
@@ -38,7 +38,7 @@ PM.findById = function(req, res) {
 
 PM.findAll = function(req, res) {
     PM.db.collection('posts', function(err, collection) {
-        collection.find().toArray(function(err, items) {
+        collection.find().sort({pubDate:-1}).toArray(function(err, items) {
 //            console.log("Frank","Wang",items);
             res.send(items);
         });
