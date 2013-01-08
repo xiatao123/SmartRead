1184aaa6d68b24631e95399a4ac5ea8499909f8e
Leon Qi <qi.leon@gmail.com>
2013-01-07 22:35:33 -0800

    Fixed db duplicated problem and revised update query.

--- data_store/feedinsert.js
+++ data_store/feedinsert.js
@@ -9,18 +9,15 @@ var feedparser = require('feedparser'),
 	db = new mongo.Db("smartreaddb", new mongo.Server(host, port, {}), {safe:false});
 
 //var tags;
-// function getTags(link){
+function getTags(link, fn){
 
-// 	var tags;
-// 	request(link, function(error, response, body) {
-// 		if (error) throw error
-//         var $ = cheerio.load(body);
-//         tags = $('meta[name=keywords]').attr('content');
-       
-//     });
-//     console.log(tags);
-//     return tags;
-// };
+	request(link, function(error, response, body) {
+		if (error) throw error
+        var $ = cheerio.load(body);
+        fn($('meta[name=keywords]').attr('content'));       
+    });
+
+};
 
 function callback(error, meta, articles){
   if (error) {
@@ -41,13 +38,37 @@ function callback(error, meta, articles){
     //   			});
 				// //var tags = getTags(article.link);
 
+				// (function getTags(){
+
+				// 	var tags;
+				// 	request(article.link, function(error, response, body) {
+				// 		if (error) throw error
+				// 	    var $ = cheerio.load(body);
+				// 	    tags = $('meta[name=keywords]').attr('content');
+					       
+				// 	});
+				// 	console.log(tags);
+				// 	return tags;
+				// } ());
+
+
+			    	// request(article.link, function(error, response, body) {
+	       //  			var $ = cheerio.load(body);
+	       //    			var tags = $('meta[name=keywords]').attr('content');
+
+	       //    			collection.findAndModify({_id:id}, {$set:{tags:tags}}, function(error, doc){
+	       //    				console.log("tags added!");
+	       //    			});
+	      	// 		});	
+
+
+
   				var $ = cheerio.load(article.description);
 			    var imgurl = $('img').first().attr('src');
 				var id = new mongo.ObjectID();
 				var filename = id + '.' + imgurl.split('.').pop();	
 				
 				collection.update({
-					pubdate: article.pubdate,
 					guid: article.guid
 				}, {
 					_id: id,
@@ -57,17 +78,32 @@ function callback(error, meta, articles){
 					description: article.summary,
 					content: article.description,
 					pubDate: article.pubdate,
+					date: article.date,
 					guid: article.guid,
 					author: article.author,	
 					comments: article.comments,
+					tags: article.categories,
 					//: config.category[meta.link],
-					//keywords: tags,
+					//tags: "",
 					picture: imgurl				
 				}, {
 					upsert: true
 				}, function(){
-					console.log(id + " with url=" + article.guid + " successfully inserted or updated!");					
-				});	
+					console.log(id + " with url=" + article.guid + " successfully inserted or updated!");	
+
+				});
+
+				// getTags(article.link, function(keywords){
+				// 	console.log("curent id is " +id + ", and tags is " + keywords);
+
+				// 	collection.update({
+				// 		link: article.link
+				// 	}, { $set:{ tags: keywords }			
+				// 	}, function(){
+				// 		console.log("Updated tags for id " + id);	
+
+				// 	});
+				// });				
 
 				//insert images to db
 				  // Open a new file
@@ -91,7 +127,7 @@ function callback(error, meta, articles){
   }//else
 }//callback
 
-feedparser.parseUrl(config.sites.tech.url2, callback);
+feedparser.parseUrl(config.sites.tech.url1, callback);
 // for(var i = 0; i <urllist.length; i++){
 // 	feedparser.parseUrl(urllist[i], callback);
 // }
