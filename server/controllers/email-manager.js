0499208bc33d50d26fecee7d67ca873f106a9fb5
Frank Wang <frankyanwang@gmail.com>
2013-01-12 23:51:44 -0800

    Use production settings for Heroku

--- server/controllers/email-manager.js
+++ server/controllers/email-manager.js
@@ -2,6 +2,8 @@ var ES = require('../email-settings');
 var EM = {};
 module.exports = EM;
 
+var HOST = process.env.NODE_ENV === "production" ? "http://smartread.herokuapp.com" : "http://localhost:3000";
+
 EM.server = require("emailjs/email").server.connect({
 
     host 	    : ES.host,
@@ -24,7 +26,7 @@ EM.dispatchResetPasswordLink = function(account, callback)
 
 EM.composeResetPasswordEmail = function(o)
 {
-    var link = 'http://localhost:3000/reset-password?e='+o.email+'&p='+o.pass;
+    var link = HOST + '/reset-password?e='+o.email+'&p='+o.pass;
     var html = "<html><body>";
     html += "你好 "+o.name+",<br><br>";
     html += "你的用户名 :: <b>"+o.user+"</b><br><br>";
@@ -48,7 +50,7 @@ EM.dispatchSignupLink = function(email, callback)
 
 EM.composeSignupEmail = function(email)
 {
-    var link = 'http://localhost:3000/#signup/'+email;
+    var link = HOST + '/#signup/'+email;
     var html = "<html><body>";
     html += "你好,<br><br>";
     html += "欢迎你申请尝试悦读网，希望它能伴随你成长！</b><br><br>";
--- server/db-settings.js
+++ server/db-settings.js
@@ -1,15 +1,19 @@
-//module.exports = {
-//    host: 'linus.mongohq.com',
-//    port: 10064,
-//    db: 'smartreaddb',
-//    username: 'smartread',
-//    password: 'yuedu2012'
-//};
-
-module.exports = {
-    host: 'localhost',
-    port: 27017,
-    db: 'smartreaddb',
-    username: '',
-    password: ''
-};
+if(process.env.NODE_ENV === "production"){
+    console.log("use production db settings");
+    module.exports = {
+        host: 'linus.mongohq.com',
+        port: 10064,
+        db: 'smartreaddb',
+        username: 'smartread',
+        password: 'yuedu2012'
+    };
+}else{
+    console.log("use dev db settings");
+    module.exports = {
+        host: 'localhost',
+        port: 27017,
+        db: 'smartreaddb',
+        username: '',
+        password: ''
+    };
+}
