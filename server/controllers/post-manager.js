18096467601abf0cf19f96b31a1a53e88f44db03
Frank Wang <frankyanwang@gmail.com>
2013-01-20 00:00:24 -0800

    Use top_stories table now. Also commented out unused code.

--- server/controllers/post-manager.js
+++ server/controllers/post-manager.js
@@ -10,11 +10,13 @@ var dp = new DataProvider();
 
 PM.db = dp.db;
 PM.posts =  PM.db.collection('posts');
+PM.topStories =  PM.db.collection('top_stories');
+
 
 module.exports = PM;
 
 PM.findById = function(storyId, callback) {
-    PM.posts.findOne({'_id':new BSON.ObjectID(storyId)}, function(err, item) {
+    PM.topStories.findOne({'_id':new BSON.ObjectID(storyId)}, function(err, item) {
         if (err){
             callback('story-not-found');
         }	else{
@@ -24,7 +26,7 @@ PM.findById = function(storyId, callback) {
 };
 
 PM.findAll = function(req, res) {
-    PM.posts.find().sort({pubDate:-1}).limit(48).toArray(function(err, items) {
+    PM.topStories.find().sort({pubDate:-1}).limit(48).toArray(function(err, items) {
 //    PM.posts.find({},{source:1,name:1,link:1,pubDate:1,guid:1,author:1,tags:1,picture:1,source:1}).sort({pubDate:-1}).toArray(function(err, items) {
         _.each(items, function(value, index){
             value['pubDate'] = Utils.getTimeAgo(value['pubDate']);
@@ -33,45 +35,45 @@ PM.findAll = function(req, res) {
     });
 };
 
-PM.addPost = function(req, res) {
-    var post = req.body;
-    console.log('Adding post: ' + JSON.stringify(post));
-    PM.posts.insert(post, {safe:true}, function(err, result) {
-        if (err) {
-            res.send({'error':'An error has occurred'});
-        } else {
-            console.log('Success: ' + JSON.stringify(result[0]));
-            res.send(result[0]);
-        }
-    });
-};
-
-PM.updatePost = function(req, res) {
-    var id = req.params.id;
-    var post = req.body;
-    delete post._id;
-    console.log('Updating post: ' + id);
-    console.log(JSON.stringify(post));
-    PM.posts.update({'_id':new BSON.ObjectID(id)}, post, {safe:true}, function(err, result) {
-        if (err) {
-            console.log('Error updating post: ' + err);
-            res.send({'error':'An error has occurred'});
-        } else {
-            console.log('' + result + ' document(s) updated');
-            res.send(post);
-        }
-    });
-};
-
-PM.deletePost = function(req, res) {
-    var id = req.params.id;
-    console.log('Deleting post: ' + id);
-    PM.posts.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-        if (err) {
-            res.send({'error':'An error has occurred - ' + err});
-        } else {
-            console.log('' + result + ' document(s) deleted');
-            res.send(req.body);
-        }
-    });
-};
+//PM.addPost = function(req, res) {
+//    var post = req.body;
+//    console.log('Adding post: ' + JSON.stringify(post));
+//    PM.posts.insert(post, {safe:true}, function(err, result) {
+//        if (err) {
+//            res.send({'error':'An error has occurred'});
+//        } else {
+//            console.log('Success: ' + JSON.stringify(result[0]));
+//            res.send(result[0]);
+//        }
+//    });
+//};
+//
+//PM.updatePost = function(req, res) {
+//    var id = req.params.id;
+//    var post = req.body;
+//    delete post._id;
+//    console.log('Updating post: ' + id);
+//    console.log(JSON.stringify(post));
+//    PM.posts.update({'_id':new BSON.ObjectID(id)}, post, {safe:true}, function(err, result) {
+//        if (err) {
+//            console.log('Error updating post: ' + err);
+//            res.send({'error':'An error has occurred'});
+//        } else {
+//            console.log('' + result + ' document(s) updated');
+//            res.send(post);
+//        }
+//    });
+//};
+//
+//PM.deletePost = function(req, res) {
+//    var id = req.params.id;
+//    console.log('Deleting post: ' + id);
+//    PM.posts.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+//        if (err) {
+//            res.send({'error':'An error has occurred - ' + err});
+//        } else {
+//            console.log('' + result + ' document(s) deleted');
+//            res.send(req.body);
+//        }
+//    });
+//};
